86221b6675fd3036ffe6320cd33585380effd86c
jota-br <ojcroveda@gmail.com>
2024-10-23 19:13:29 -0300

    Polymorphism

---
+++ PolymorphismChallenge2/Ford.java
@@ -0,0 +1,21 @@
+public class Ford extends Car {
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String accelerate() {
+        return this.getClass().getSimpleName() + " -> accelerate";
+    }
+
+    @Override
+    public String brake() {
+        return this.getClass().getSimpleName() + " -> brake";
+    }
+
+    @Override
+    public String startEngine() {
+        return this.getClass().getSimpleName() + " -> startEngine";
+    }
+}
