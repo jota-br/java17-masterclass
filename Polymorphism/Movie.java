03601939c6bae4ee6e5372bcc5868184d3bdb4d0
jota-br <ojcroveda@gmail.com>
2024-10-22 21:12:05 -0300

    Polymorphism

---
+++ Polymorphism/Movie.java
@@ -0,0 +1,64 @@
+public class Movie {
+    
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(this.title + " is a " + instanceType + " film");
+    }
+
+    // Factory Method
+    public static Movie getMovie(String type, String title) {
+
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+}
+
+class Adventure extends Movie {
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Pleasant Scene", "Scary Music", "Something Band Happens");
+    }
+}
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Something Funny Happens", "Something Even Funnier Happens", "LOL");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Fiction", "Science Fiction", "Mind Blowing");
+    }
+}
